704949f578a943df6ae96c386c8dcb059dfaffb6
Naveen <kumarnav909@gmail.com>
2023-12-04 18:15:39 +0530

    lab6 done

--- src/Counter.js
+++ src/Counter.js
@@ -2,23 +2,15 @@
 import React, { useState } from 'react';
 
 export default function Counter() {
+  // Step 1: Declare state variable and a function to update it
   const [count, setCount] = useState(0);
-  const [inputValue, setInputValue] = useState('');
 
   return (
     <div>
       <h2>Counter: {count}</h2>
+      {/* Step 2: Update state on button click */}
       <button onClick={() => setCount(count + 1)}>Increment</button>
       <button onClick={() => setCount(count - 1)}>Decrement</button>
-
-      {/* Step 4: Add an input field and update state based on input value */}
-      <input
-        type="text"
-        placeholder="Hey I am Naveen Kumar"
-        value={inputValue}
-        onChange={(e) => setInputValue(e.target.value)}
-      />
-      <p>You typed: {inputValue}</p>
     </div>
   );
 }
